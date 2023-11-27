195b871471b677d83bd3825a66baa3b8c5e06b4d
Sami Karkinen <sami.karkinen@codemen.fi>
2023-11-27 23:05:17 +0200

    Added file argument parsing

--- apply_modifier_and_export.py
+++ apply_modifier_and_export.py
@@ -1,1 +1,10 @@
-from bpy import ops
+# from bpy import ops
+from bpy.ops.wm import open_mainfile
+from argparse import ArgumentParser
+
+arg_parser = ArgumentParser()
+arg_parser.add_argument("-f", "--file", type=str, help="File to open")
+
+args = arg_parser.parse_args()
+
+open_mainfile(filename=args.file)
